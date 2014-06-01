75c8b533a6a87c72def969091752116e9b6f5149
Fujimura Daisuke <me@fujimuradaisuke.com>
2014-06-01 18:15:12 +0900

    Second commit

--- git-gsub.gemspec
+++ git-gsub.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |spec|
   spec.version       = Git::Gsub::VERSION
   spec.authors       = ["Fujimura Daisuke"]
   spec.email         = ["me@fujimuradaisuke.com"]
-  spec.summary       = %q{TODO: Write a short summary. Required.}
-  spec.description   = %q{TODO: Write a longer description. Optional.}
+  spec.summary       = %q{git-gsub}
+  spec.description   = %q{git-gusb}
   spec.homepage      = ""
   spec.license       = "MIT"
 
--- lib/git/gsub.rb
+++ lib/git/gsub.rb
@@ -1,7 +1,27 @@
 require "git/gsub/version"
+require 'shellwords'
 
 module Git
   module Gsub
-    # Your code goes here...
+    def self.run
+      case ARGV.first
+      when '-v', '--version'
+        version
+      else
+        gsub *ARGV
+      end
+    end
+
+    def self.version
+      puts Git::Gsub::VERSION
+    end
+
+    def self.gsub *args
+      from, to, path, = args
+
+      target_files = (`git grep -l #{from} #{path}`).each_line.map(&:chomp).join ' '
+
+      system %|gsed -i s/#{Shellwords.escape from}/#{Shellwords.escape to}/g #{target_files}|
+    end
   end
 end
