d6eb71b0612366548e7b963ad87130d8913d1a11
Mitsutaka Mimura <takkanm@gmail.com>
2014-06-30 17:58:59 +0900

    support gsub
    I don't use gsub. but, I want to use git-gsub.

--- lib/git/gsub.rb
+++ lib/git/gsub.rb
@@ -21,7 +21,18 @@ module Git
 
       target_files = (`git grep -l #{from} #{path}`).each_line.map(&:chomp).join ' '
 
-      system %|gsed -i s/#{Shellwords.escape from}/#{Shellwords.escape to}/g #{target_files}|
+      if system_support_gsed?
+        system %|gsed -i s/#{Shellwords.escape from}/#{Shellwords.escape to}/g #{target_files}|
+      else
+        system %|sed -i -e s/#{Shellwords.escape from}/#{Shellwords.escape to}/g #{target_files}|
+      end
+    end
+
+    private
+
+    def self.system_support_gsed?
+      `which gsed`
+      $?.success?
     end
   end
 end
