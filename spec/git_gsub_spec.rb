e763949a181c51bdce0eedcffbf8183b6685a83e
Fujimura Daisuke <me@fujimuradaisuke.com>
2014-07-23 00:33:40 +0900

    Add spec

---
+++ spec/git_gsub_spec.rb
@@ -0,0 +1,35 @@
+require 'rspec'
+require 'tmpdir'
+require 'git/gsub'
+require 'pry'
+
+describe 'git-gsub' do
+  def run_in_directory_with_a_file content
+    Dir.mktmpdir do |dir|
+      filename = "FOO"
+      Dir.chdir dir do
+        `echo "#{content}" >> #{filename}`
+        `git init`
+        `git add .`
+        `git commit -m init`
+        yield filename
+      end
+    end
+  end
+
+  it 'should substitute files' do
+    run_in_directory_with_a_file "Git Subversion Bzr" do |filename|
+      Git::Gsub.gsub "Bzr", "Mercurial"
+      file = File.read(filename).chomp
+      expect(file).to eq "Git Subversion Mercurial"
+    end
+  end
+
+  it 'should escape well' do
+    run_in_directory_with_a_file %|<h1 *class="foo">| do |filename|
+      Git::Gsub.gsub %|<h1 class="foo">|, %|<h1 class="bar">|
+      file = File.read(filename).chomp
+      expect(file).to eq %|<h1 class="bar">|
+    end
+  end
+end
