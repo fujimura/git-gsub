a0812a389223ebe6b78bedd6beaad72f916c98a9
Fujimura Daisuke <me@fujimuradaisuke.com>
2017-04-24 14:26:33 +0900

    Allow multiple target directories

--- lib/git/gsub.rb
+++ lib/git/gsub.rb
@@ -17,7 +17,7 @@ module Git
     end
 
     def self.gsub *args
-      from, to, path, = args.map do |arg|
+      from, to, paths = args.map do |arg|
         Shellwords.escape arg if arg
       end
 
@@ -25,7 +25,7 @@ module Git
         abort "No argument to gsub was given"
       end
 
-      target_files = (`git grep -l #{from} #{path}`).each_line.map(&:chomp).join ' '
+      target_files = (`git grep -l #{from} #{paths.join ' '}`).each_line.map(&:chomp).join ' '
 
       if system_support_gsed?
         system %|gsed -i s/#{from}/#{to}/g #{target_files}|
