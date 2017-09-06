c0ad7954d5b434b9624cde08a56425ec97aad5af
Fujimura Daisuke <me@fujimuradaisuke.com>
2017-09-06 11:50:22 +0900

    Make it explicit

--- spec/git_gsub_spec.rb
+++ spec/git_gsub_spec.rb
@@ -13,51 +13,51 @@ describe 'git-gsub' do
         `git config --local user.name "Your Name"`
         `git add .`
         `git commit -m init`
-        yield filename
+        yield
       end
     end
   end
 
   it 'should substitute files' do
-    run_in_directory_with_a_file 'README.md', 'Git Subversion Bzr' do |filename|
+    run_in_directory_with_a_file 'README.md', 'Git Subversion Bzr' do
       Git::Gsub.run %w[Bzr Mercurial]
-      file = File.read(filename).chomp
+      file = File.read('README.md').chomp
       expect(file).to eq 'Git Subversion Mercurial'
     end
   end
 
   it 'should substitute files with case conversion' do
-    run_in_directory_with_a_file 'README.md', 'GitGsub git_gsub git-gsub' do |filename|
+    run_in_directory_with_a_file 'README.md', 'GitGsub git_gsub git-gsub' do
       Git::Gsub.run %w[GitGsub SvnGsub --camel --kebab --snake]
-      file = File.read(filename).chomp
+      file = File.read('README.md').chomp
       expect(file).to eq 'SvnGsub svn_gsub svn-gsub'
     end
   end
 
   it 'should escape well' do
-    run_in_directory_with_a_file 'README.md', %(<h1 class="foo">) do |filename|
+    run_in_directory_with_a_file 'README.md', %(<h1 class="foo">) do
       Git::Gsub.run [%(<h1 class="foo">), %(<h1 class="bar">)]
-      file = File.read(filename).chomp
+      file = File.read('README.md').chomp
       expect(file).to eq %(<h1 class="bar">)
     end
   end
 
   it 'should not create backup file' do
-    run_in_directory_with_a_file 'README.md', 'Git Subversion Bzr' do |_filename|
+    run_in_directory_with_a_file 'README.md', 'Git Subversion Bzr' do
       Git::Gsub.run %w[Bzr Darcs]
       expect(`ls`).to eql "README.md\n"
     end
   end
 
   it 'should rename with --rename' do
-    run_in_directory_with_a_file 'README-git_gsub.md', 'GitGsub git_gsub git-gsub' do |_filename|
+    run_in_directory_with_a_file 'README-git_gsub.md', 'GitGsub git_gsub git-gsub' do
       Git::Gsub.run %w[GitGsub SvnGsub --snake --rename]
       expect(`ls`).to eql "README-svn_gsub.md\n"
     end
   end
 
   it 'should do nothing if no file found' do
-    run_in_directory_with_a_file 'README-git_gsub.md', 'GitGsub git_gsub git-gsub' do |_filename|
+    run_in_directory_with_a_file 'README-git_gsub.md', 'GitGsub git_gsub git-gsub' do
       expect {
         Git::Gsub.run %w[Atlanta Chicago --snake --rename]
       }.not_to raise_error
